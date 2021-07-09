8516d066fc7456851ab5090afef51e5e5421736e
Yoav Levy <yoav.levy@fiverr.com>
2021-07-10 00:23:21 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Water Mark Adder
+
+A python script that adds water mark to all images in the directory `images/` and put them in the directory `water_marked_images`.
+
+### Restrictions
+
+* All of the images must be in the same size (The example files are in the size of 500 x 333 pixels)
+* To run the program one must have python3, and opencv-python module installed.
+
+## How to run?
+
+1. Clone the repo.
+2. Navigate to the directory.
+3. Open the Shell and run `python water_mark_adder.py`.
+4. Have fun :)
---
+++ water_mark_adder.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import os
+import glob
+
+alpha = 0.8
+failure = -1
+
+
+def listdir_nohidden(path):
+    return glob.glob(os.path.join(path, '*'))
+
+
+def main():
+    images = listdir_nohidden('./images')
+    for image in images:
+        image_name = image.split('/')[2]
+        print(image_name)
+        src1 = cv.imread(cv.samples.findFile(image))
+        src2 = cv.imread(cv.samples.findFile('copy_right.jpeg'))
+        if src1 is None:
+            print("Error loading src1")
+            exit(failure)
+        elif src2 is None:
+            print("Error loading src2")
+            exit(failure)
+        # Blending Images
+        beta = (1.0 - alpha)
+        dst = cv.addWeighted(src1, alpha, src2, beta, 0.0)
+        cv.imwrite('./water_marked_images/' + image_name, dst)
+
+
+if __name__ == '__main__':
+    main()
